f9b6d26dab2f03d3bdfcda4a636b2a6711981722
Josh Rueschenberg <joshru@uw.edu>
2015-04-13 11:41:51 -0700

    no

---
+++ app/src/main/java/com/tcss450/moneyteam/mvc_practice/Course.java
@@ -0,0 +1,18 @@
+package com.tcss450.moneyteam.mvc_practice;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Josh Rueschenberg on 4/13/2015.
+ */
+public class Course {
+    public String mCID;
+    public String mSDes;
+    public String mLDes;
+    public ArrayList<Course> mPrereqs;
+
+
+    public Course() {
+
+    }
+}
---
+++ app/src/main/java/com/tcss450/moneyteam/mvc_practice/Instructor.java
@@ -0,0 +1,23 @@
+package com.tcss450.moneyteam.mvc_practice;
+
+import java.net.URI;
+
+/**
+ * Created by Josh Rueschenberg on 4/13/2015.
+ */
+public class Instructor {
+    public String mName;
+    public String mTitle;
+    public URI mImage;
+    public String mEmail;
+    public String mPhone;
+    public String mOffice;
+    public String mBio;
+
+
+    public Instructor() {
+
+    }
+
+
+}
