db4596926a6cd14b708e5ebc067e9f9c748e391f
Brandon Bell <brandb94@uw.edu>
2015-04-14 09:17:20 -0700

    Added toast for creating a course, persistent data when rotating device, persistent data when pressing home and returning to the app

--- app/src/main/java/com/tcss450/moneyteam/mvc_practice/CourseActivity.java
+++ app/src/main/java/com/tcss450/moneyteam/mvc_practice/CourseActivity.java
@@ -1,17 +1,93 @@
 package com.tcss450.moneyteam.mvc_practice;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class CourseActivity extends ActionBarActivity {
+
+    EditText mCID;
+    EditText mShortDesc;
+    EditText mLongDesc;
+    EditText mPreReqs;
+    Button mCreateCourseButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        mCID = (EditText)findViewById(R.id.CID_text);
+        mShortDesc = (EditText)findViewById(R.id.short_desc_text);
+        mLongDesc = (EditText)findViewById(R.id.long_desc_text);
+        mPreReqs = (EditText)findViewById(R.id.prereq_text);
+        mCreateCourseButt = (Button)findViewById(R.id.create_course_button);
+
+
+        mCreateCourseButt.setOnClickListener(new SubmitHandler());
+        /*mCreateCourseButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Course course = new Course(mCID.getText().toString(), mShortDesc.getText().toString(),
+                        mLongDesc.getText().toString(), mPreReqs.getText().toString());
+
+                Toast toasty = Toast.makeText(getApplicationContext(), "Course created, bruh", Toast.LENGTH_SHORT);
+                toasty.show();
+
+            }
+        });*/
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        SharedPreferences sharedPrefs = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor eddy = sharedPrefs.edit();
+
+        mCID.setText(sharedPrefs.getString(getString(R.string.saved_course_ID),
+                getString(R.string.default_value)));
+        mShortDesc.setText(sharedPrefs.getString(getString(R.string.saved_short_desc),
+                getString(R.string.default_value)));
+        mLongDesc.setText(sharedPrefs.getString(getString(R.string.saved_long_desc),
+                getString(R.string.default_value)));
+        mPreReqs.setText(sharedPrefs.getString(getString(R.string.saved_prereqs),
+                getString(R.string.default_value)));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+       /* SharedPreferences sharedPrefs = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor eddy = sharedPrefs.edit();
+        eddy.putString(getString(R.string.saved_course_ID), mCID.getText().toString());
+        eddy.putString(getString(R.string.saved_short_desc), mShortDesc.getText().toString());
+        eddy.putString(getString(R.string.saved_long_desc), mLongDesc.getText().toString());
+        eddy.putString(getString(R.string.saved_prereqs), mPreReqs.getText().toString());
+
+        eddy.commit();*/
+        saveData();
+
+
+
+
+        super.onSaveInstanceState(outState);
+
     }
 
     @Override
@@ -34,5 +110,29 @@ public class CourseActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void saveData() {
+        SharedPreferences sharedPrefs = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor eddy = sharedPrefs.edit();
+        eddy.putString(getString(R.string.saved_course_ID), mCID.getText().toString());
+        eddy.putString(getString(R.string.saved_short_desc), mShortDesc.getText().toString());
+        eddy.putString(getString(R.string.saved_long_desc), mLongDesc.getText().toString());
+        eddy.putString(getString(R.string.saved_prereqs), mPreReqs.getText().toString());
+
+        eddy.commit();
+    }
+
+    private class SubmitHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Course course = new Course(mCID.getText().toString(), mShortDesc.getText().toString(),
+                    mLongDesc.getText().toString(), mPreReqs.getText().toString());
+
+            Toast toasty = Toast.makeText(getApplicationContext(), "Course created, bruh", Toast.LENGTH_SHORT);
+            toasty.show();
+
+        }
+
     }
 }
