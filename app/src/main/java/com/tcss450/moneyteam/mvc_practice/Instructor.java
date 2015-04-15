80bcc8717b4ebe6f81474c68c86bac3c1d2c7cfb
Brandon Bell <brandb94@uw.edu>
2015-04-15 12:22:27 -0700

    Added serialization behavior to instructor

--- app/src/main/java/com/tcss450/moneyteam/mvc_practice/Instructor.java
+++ app/src/main/java/com/tcss450/moneyteam/mvc_practice/Instructor.java
@@ -1,17 +1,26 @@
 package com.tcss450.moneyteam.mvc_practice;
 
-import java.net.URI;
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 /**
  * Created by Josh Rueschenberg on 4/13/2015.
  */
-public class Instructor {
-    public String mName;
-    public String mTitle;
-    public String mEmail;
-    public String mPhone;
-    public String mOffice;
-    public String mBio;
+public class Instructor implements Serializable {
+    private static final long serialVersionUID = 0L;
+    private static final String objectFile = "instructor";
+    private String mName;
+    private String mTitle;
+    private String mEmail;
+    private String mPhone;
+    private String mOffice;
+    private String mBio;
 //    public URI mImage;
 
     public Instructor(final String theName, final String theTitle, final String theEmail,
@@ -24,6 +33,41 @@ public class Instructor {
         mBio = theBio;
 
     }
+     public static void writeToFile(Context ctx, Instructor instructor) {
+        FileOutputStream fOutputStream;
+        ObjectOutputStream objectOutputStream;
+        try {
+
+            fOutputStream = ctx.openFileOutput(objectFile, Context.MODE_PRIVATE);
+            objectOutputStream = new ObjectOutputStream(fOutputStream);
+            objectOutputStream.writeObject(instructor);
+            objectOutputStream.close();
+            fOutputStream.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        //out.writeObject(this);
+        //out.flush();
+    }
+    public static Instructor readObject(Context ctx) throws IOException, ClassNotFoundException {
+        // = (Instructor)in.readObject();
+        //return  returnInstructor;
+        FileInputStream FIS;
+        ObjectInputStream OIS;
+        Instructor instructor = null;
+        try {
+
+            FIS = ctx.openFileInput(objectFile);
+            OIS = new ObjectInputStream(FIS);
+            instructor = (Instructor) OIS.readObject();
+            OIS.close();
+            FIS.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return instructor;
+    }
+
 
 
 }
